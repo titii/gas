e43cab5331fb475f283a8c203a158e7154fce2d2
shota.onodera <shota.titi.0703@gmail.com>
2020-07-24 17:38:54 +0900

    Tidy up indentation.

--- bos/growth_stock.js
+++ bos/growth_stock.js
@@ -13,30 +13,30 @@ function deleteTriggerAll() {
 }
 
 function getLatestRevenue() {
-	createTrigger();
+  createTrigger();
 }
 
 function getDataFromOtherSite() {
-	var sheet = SpreadsheetApp.getActiveSheet(); 
-	var counter = sheet.getRange(3, 24);
-	var count = counter.getValue() + 1;
-	counter.setValue(count);
-	var cell = sheet.getRange(count + 3, 4)
-	var ticker = cell.getValue().toString();
-	var financialsUrl = "https://www.marketwatch.com/investing/stock/" + ticker + "/financials";
+  var sheet = SpreadsheetApp.getActiveSheet(); 
+  var counter = sheet.getRange(3, 24);
+  var count = counter.getValue() + 1;
+  counter.setValue(count);
+  var cell = sheet.getRange(count + 3, 4)
+  var ticker = cell.getValue().toString();
+  var financialsUrl = "https://www.marketwatch.com/investing/stock/" + ticker + "/financials";
   financials = UrlFetchApp.fetch(financialsUrl).getContentText();
   var revenue = getRevenue(financials);
   sheet.getRange(count + 3, 17).setValue(revenue);
   count++;
   if (cell.isBlank()) {
-  	deleteTriggerAll();
+    deleteTriggerAll();
   }
 }
 
 function getRevenue(financials) {
-	var htmlData = financials.match(/<tr class="partialSum">[\s\S]*<\/tr/g)[0];
-	var revenueRegExp = /<div class="miniGraph" data-chart=".*(.*?)]}/;
-	var revenueData = htmlData.match(revenueRegExp)[0];
-	var latestRevenue = revenueData.match(/\d+/g)[4];
-	return latestRevenue;
+  var htmlData = financials.match(/<tr class="partialSum">[\s\S]*<\/tr/g)[0];
+  var revenueRegExp = /<div class="miniGraph" data-chart=".*(.*?)]}/;
+  var revenueData = htmlData.match(revenueRegExp)[0];
+  var latestRevenue = revenueData.match(/\d+/g)[4];
+  return latestRevenue;
 }
